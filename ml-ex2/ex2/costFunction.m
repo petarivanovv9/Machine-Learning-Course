e0f9a2e41a508307709baa6c66a2a4922ca23454
Petar Ivanov <pepincho95@gmail.com>
2020-04-21 21:09:06 +0300

    Add solutions to the tasks from ex2

--- ml-ex2/ex2/costFunction.m
+++ ml-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -21,9 +21,11 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X * theta);
 
+J = sum((-y' * log(h)) - ((1 - y') * log(1 - h))) / m;
 
-
+grad = (X' * (h - y)) / m;
 
 
 
--- ml-ex2/ex2/costFunctionReg.m
+++ ml-ex2/ex2/costFunctionReg.m
@@ -2,12 +2,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -18,6 +18,13 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+h = sigmoid(X * theta);
+
+J = sum((-y' * log(h)) - ((1 - y') * log(1 - h))) / m + theta(2:end)' * lambda / (2 * m);
+
+grad(1) = X(:, 1)' * (h - y) / m;
+X1 = X(:, 2:end);
+grad(2:end) = (X1' * (h - y)) / m + lambda * theta(2:end) / m;
 
 
 
